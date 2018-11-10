2e70145ae5e95f7c6ac399c7ec073d35ff941d13
Billy.Wang <wangbb@chenyee.com>
2018-11-10 18:23:09 +0800

    删除mmi多余文件，AutoMMI Makefile更新，屏蔽P编译更新mode文件

--- package/CY_AutoMMI/OM8.0/Android.mk
+++ package/CY_AutoMMI/OM8.0/Android.mk
@@ -8,6 +8,15 @@ endif
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
+GN_APK_PLATFORM_VERSION=8.0
+GN_APK_PLATFORM_VENDOR=mtk
+GN_APK_MTK_PLATFORM=6762
+#GN_ZIP_NAME_PALTFORM=6589
+# 4.1.0-3,4.2.0 (4.1.0/4.1.1/4.1.2/4.1.3/4.2.0)
+# 1280x720,800x480
+GN_APK_RESOLUTION=hdpi,mdpi,nodpi,xhdpi,xxhdpi
+GN_APK_LANGUAGE = zh_CN,en_US
+
 LOCAL_JAVA_LIBRARIES := telephony-common
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src) \
